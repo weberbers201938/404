92579e32a82d355932cd28d73f22662d6266e653
berlovesumwah <166808321+weberbers201938@users.noreply.github.com>
2024-07-14 18:21:23 +0800

    Update start.py

--- start.py
+++ start.py
@@ -1,56 +1,59 @@
-import os
 import subprocess
+import os
+import platform
 
-def check_installation(command):
+def check_command(command):
     try:
         subprocess.run([command, '--version'], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         return True
-    except subprocess.CalledProcessError:
+    except (subprocess.CalledProcessError, FileNotFoundError):
         return False
 
-def run_command(command):
-    result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    if result.returncode != 0:
-        print(f"Error running command: {command}")
-        print(result.stderr.decode())
-    else:
-        print(result.stdout.decode())
+def install_nodejs():
+    node_version = "v20.15.0"  # Specify the version you want
+    node_dist_url = f"https://nodejs.org/dist/{node_version}/node-{node_version}-linux-x64.tar.xz"
 
-def install_nvm():
-    run_command('curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash')
-    run_command('export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"')
-    run_command('[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"')
-    run_command('[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"')
+    # Download Node.js
+    print("Downloading Node.js...")
+    subprocess.run(['curl', '-o', 'node.tar.xz', node_dist_url], check=True)
 
-def install_node_and_tools():
-    run_command('nvm install node')
-    run_command('npm install -g yarn')
+    # Extract Node.js
+    print("Extracting Node.js...")
+    subprocess.run(['tar', '-xf', 'node.tar.xz'], check=True)
+
+    # Move to a directory
+    node_dir = f'node-{node_version}-linux-x64'
+    os.environ['PATH'] = os.path.abspath(node_dir + '/bin') + ':' + os.environ['PATH']
+
+    print("Node.js installed and PATH updated.")
+
+def install_yarn():
+    print("Installing Yarn...")
+    subprocess.run(['npm', 'install', '--global', 'yarn'], check=True)
+    print("Yarn installed.")
 
 def main():
-    if not check_installation('node'):
-        print("Node.js is not installed. Installing...")
-        install_nvm()
-        install_node_and_tools()
-    else:
-        print("Node.js is already installed.")
+    if not check_command('node'):
+        print("Node.js not found. Installing Node.js...")
+        install_nodejs()
 
-    if not check_installation('npm'):
-        print("npm is not installed. This should have been installed with Node.js.")
-        install_node_and_tools()
-    else:
-        print("npm is already installed.")
+    if not check_command('npm'):
+        print("npm not found. It should be installed with Node.js. Checking again...")
+        if not check_command('npm'):
+            print("npm installation failed. Exiting.")
+            return
 
-    if not check_installation('yarn'):
-        print("Yarn is not installed. Installing...")
-        run_command('npm install -g yarn')
-    else:
-        print("Yarn is already installed.")
+    if not check_command('yarn'):
+        print("Yarn not found. Installing Yarn...")
+        install_yarn()
 
+    # Run yarn install
     print("Running 'yarn install'...")
-    run_command('yarn install')
+    subprocess.run(['yarn', 'install'], check=True)
 
+    # Run npm start
     print("Running 'npm run start'...")
-    run_command('npm run start')
+    subprocess.run(['npm', 'run', 'start'], check=True)
 
 if __name__ == "__main__":
     main()
