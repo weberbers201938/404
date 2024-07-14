6aa0b3f5169a687a0825222866a2c094c4602ab0
berlovesumwah <166808321+weberbers201938@users.noreply.github.com>
2024-07-14 19:03:45 +0800

    Update console-logger.js

--- configs/catalogs/system-settings/console/console-logger.js
+++ configs/catalogs/system-settings/console/console-logger.js
@@ -1,58 +1,64 @@
 const chalk = require('chalk');
-const gradient= require('gradient-string');
+const gradient = require('gradient-string');
 const chalkAnimation = require('chalkercli');
 const color = gradient('blue', 'purple');
 const crayon = gradient('yellow', 'lime', 'green');
-const cheerful = gradient.fruit
+const cheerful = gradient.fruit;
 const crayon2 = gradient('yellow', 'lime', 'green');
 const sky2 = gradient('#3446eb', '#3455eb', '#3474eb');
-const BLUE = ('#3467eb');
+const BLUE = '#3467eb';
 const blu = gradient("#243aff", "#4687f0", "#5800d4");
-const sky = gradient('#0905ed','#346eeb', '#344feb');
+const sky = gradient('#0905ed', '#346eeb', '#344feb');
 const config = require("./../../../../config.json");
-var successColor = config.LOGGER_SETTINGS.console.success;
+
+const successColor = config.LOGGER_SETTINGS.console.success;
 const errorColor = config.LOGGER_SETTINGS.console.error;
 const warnColor = config.LOGGER_SETTINGS.console.warn;
+
+const logMessage = (color, label, text) => {
+  const message = typeof text === 'object' ? JSON.stringify(text, null, 2) : text;
+  process.stderr.write(chalk[color](label) + ` : ${message}\n`);
+};
 
 module.exports = (text, type) => {
   switch (type) {
     case "warn":
-      process.stderr.write(chalk[`${warnColor}`](config.LOGGER_SETTINGS.console.editNames.warn) + ` : ${text}\n`);
+      logMessage(warnColor, config.LOGGER_SETTINGS.console.editNames.warn, text);
       break;
     case "error":
-      process.stderr.write(chalk[`${errorColor}`](config.LOGGER_SETTINGS.console.editNames.error) + ` : ${text}\n`);
+      logMessage(errorColor, config.LOGGER_SETTINGS.console.editNames.error, text);
       break;
     case "load":
-      process.stderr.write(chalk[`${successColor}`]('new user') + `: ${text}\n`);
+      logMessage(successColor, 'new user', text);
       break;
     default:
-      process.stderr.write(chalk[`${successColor}`](type) + ` : ${text}\n`);
+      logMessage(successColor, type, text);
       break;
   }
 };
-module.exports.error = (text, type) => {
-  process.stderr.write(chalk[`${errorColor}`](config.LOGGER_SETTINGS.console.editNames.error) + ` : ${text}\n`);
+
+module.exports.error = (text) => {
+  logMessage(errorColor, config.LOGGER_SETTINGS.console.editNames.error, text);
 };
 
-module.exports.err = (text, type) => {
-  process.stderr.write(chalk[`${errorColor}`](config.LOGGER_SETTINGS.console.editNames.error) + ` : ${text}\n`);
+module.exports.err = (text) => {
+  logMessage(errorColor, config.LOGGER_SETTINGS.console.editNames.error, text);
 };
 
-module.exports.warn = (text, type) => {
-  process.stderr.write(chalk[`${warnColor}`](config.LOGGER_SETTINGS.console.editNames.warn) + ` : ${text}\n`);
+module.exports.warn = (text) => {
+  logMessage(warnColor, config.LOGGER_SETTINGS.console.editNames.warn, text);
 };
-
 
 module.exports.loader = (data, option) => {
   switch (option) {
     case "warn":
-      process.stderr.write(chalk[`${warnColor}`](config.LOGGER_SETTINGS.console.editNames.warn) + ` : ${data}\n`);
+      logMessage(warnColor, config.LOGGER_SETTINGS.console.editNames.warn, data);
       break;
     case "error":
-      process.stderr.write(chalk[`${errorColor}`](`${config.LOGGER_SETTINGS.console.editNames.error}`) + ` : ${data}\n`);
+      logMessage(errorColor, config.LOGGER_SETTINGS.console.editNames.error, data);
       break;
     default:
-      process.stderr.write(chalk[`${successColor}`](`${config.LOGGER_SETTINGS.console.editNames.success}`) + ` : ${data}\n`);
+      logMessage(successColor, config.LOGGER_SETTINGS.console.editNames.success, data);
       break;
   }
-}
+};
